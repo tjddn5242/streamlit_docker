36c61cf56be4dad41f0a8e7bbe23bac081097829
swkim <tjddn5242@gmail.com>
2025-05-05 01:02:35 +0900

    no message

--- app.py
+++ app.py
@@ -6,13 +6,4 @@ import scipy
 import plotly
 import streamlit as st
 
-def main():
-    st.write(np.__version__)
-    st.write(pd.__version__)
-    st.write(matplotlib.__version__)
-    st.write(sklearn.__version__)
-    st.write(scipy.__version__)
-    st.write(plotly.__version__)
-
-if __name__ == "__main__":
-    main()
+st.title("Hello World")
